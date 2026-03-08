2e8d9f15b2636f0d170cc94527e83193a6fbf46b
Carlos ZHOU <carloszhoucy@gmail.com>
2026-03-08 14:48:55 +0800

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-const apiHost = process.env.VITE_API_HOST || '192.168.0.101'
+const apiHost = process.env.VITE_API_HOST || '127.0.0.1'
 const apiPort = process.env.VITE_API_PORT || '8001'
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     port: 8000,
     host: '0.0.0.0',
-    allowedHosts: ['wulianxx.com','192.168.0.101'],
+    allowedHosts: ['12.70.0.1'],
     proxy: {
       // 代理所有 /api 请求到后端 8001 端口
       '/api': {
